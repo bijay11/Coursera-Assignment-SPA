2a8e29458493fa0ae75509cc515f0e7ede7a3824
bijay11 <tmg_bijay11@yahoo.com>
2016-10-03 22:03:07 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+var app = angular.module('LunchCheck', []);
+app.controller('LunchCheckController',  ['$scope', function($scope) {
+    $scope.post = {dishNames: null};
+    $scope.comma = ',';
+
+
+    $scope.CheckDish = function (dishToSplit, separator) {
+  var dishCount = dishToSplit.split(separator);
+
+if (dishCount.length > 0 && dishCount.length <= 3){
+  $scope.message = "Enjoy";
+}else if(dishCount.length > 3){
+  $scope.message = "Too mch";
+} else {
+    $scope.message = "Please enter data first";
+}
+
+
+}
+
+}]);
