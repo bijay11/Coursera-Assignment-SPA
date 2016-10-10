6f0a39fb75b862ac1af4570f7d99b9c2f36be336
bijay11 <tmg_bijay11@yahoo.com>
2016-10-10 16:12:29 -0400

    Update app.js

--- SPA-Assignment-Module-2/app.js
+++ SPA-Assignment-Module-2/app.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    angular.module('ShoppingListApp', [])
+    angular.module('ShoppingListCheckOff', [])
         .controller('AlreadyBoughtController', AlreadyBoughtController)
         .controller('ToBuyController', ToBuyController)
         .service('ItemListService', ItemListService);
